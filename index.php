4d9cee6bcd9dea2a887150d1f55caaa3cdc686c8
Pedro Lucas <pedro.lucas.fl@hotmail.com>
2022-08-11 21:35:22 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use Hcode\Page;
 use Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -22,10 +23,41 @@ $app->get('/', function() {
 //Rota para painel admin.
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
     
+});
+
+//Rota para painel login admin.
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+    
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("location: /admin");
+	exit;
+    
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 
